cc0ac120bbe5fa23fdd9ceaba3a3e1ac7d5a80a5
Wangjinquan <wangjinquan@hhdd.com>
2022-10-11 20:29:29 +0800

    formatter

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,17 +3,10 @@ import theme from "./theme.js";
 
 export default defineUserConfig({
   base: "/",
-
   dest: "./dist",
-  locales: {
-    "/": {
-      lang: "zh-CN",
-      title: "漂流小岛",
-      description: "欢迎来到漂流小岛",
-    }
-  },
-
-  theme,
-
+  lang: "zh-CN",
+  title: "漂流小岛",
+  description: "欢迎来到漂流小岛",
   shouldPrefetch: false,
+  theme,
 });
